3a481d655f24cb1aee87baf1fe4a79af3a1200b4
yo-soy-dev <devanshtiwari817@gmail.com>
2025-07-10 20:52:41 +0530

    project completed

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectDB()
 await connectCloudinary()
 
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-lake-three.vercel.app'];
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
